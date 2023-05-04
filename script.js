88d5a3345728de1f941cb597cf89335c293aff18
Ayosef <syrivera@uc.cl>
2023-05-04 18:01:19 -0400

    Add a form for the user to input books

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const table = document.querySelector('#table');
+const form = document.querySelector('.form');
+// const title = document.querySelector('#title');
+// const author = document.querySelector('#author');
+// const pages = document.querySelector('#pages');
+// const read = document.querySelector('#read');
 
 let myLibrary = [];
 
@@ -9,15 +14,18 @@ function Book(title, author, pages, read) {
     this.read = read
 }
 
-function addBookToLibrary() {
-    title = prompt("Enter book title")
-    author = prompt("Enter book author")
-    pages = prompt("Enter book pages")
-    read = prompt("Enter book read")
+function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
 
-
-console.log(table);
-console.log(myLibrary);
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    title = e.currentTarget.title.value;
+    author = e.currentTarget.author.value;
+    pages = e.currentTarget.pages.value;
+    read = e.currentTarget.read.checked;
+    addBookToLibrary(title, author, pages ,read);
+    console.log(e.currentTarget.author.value);
+    console.log(myLibrary);
+})
