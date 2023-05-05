87acabd83a6b09733808759b73aeaa93b2387088
Ayosef <syrivera@uc.cl>
2023-05-05 09:07:44 -0400

    Add a function to clear and update the table

--- script.js
+++ script.js
@@ -20,14 +20,33 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
-function addRows() {
-    let trow = document.createElement('tr');
-    for (let i=0 ; i<4; i++) {
-        let tdata = document.createElement('td');
-        trow.appendChild(tdata);
-    }
-    tbody.appendChild(trow);
+function updateTable(library) {
+    library.forEach(book => {
+        let row = tbody.insertRow();
+        let title = row.insertCell(0);
+        let author = row.insertCell(1);
+        let pages = row.insertCell(2);
+        let read = row.insertCell(3);
+        title.textContent = book.title
+        author.textContent = book.author
+        pages.textContent = book.pages
+        read.textContent = book.read
+        console.log(row);
+    });
 }
+
+function clearTable(){
+    tbody.innerHTML = '';
+}
+
+// function addRows() {
+//     let trow = document.createElement('tr');
+//     for (let i=0 ; i<4; i++) {
+//         let tdata = document.createElement('td');
+//         trow.appendChild(tdata);
+//     }
+//     tbody.appendChild(trow);
+// }
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -36,6 +55,7 @@ form.addEventListener('submit', (e) => {
     pages = e.currentTarget.pages.value;
     read = e.currentTarget.read.checked;
     addBookToLibrary(title, author, pages ,read);
-    addRows();
+    clearTable();
+    updateTable(myLibrary);
     console.log(myLibrary);
 })
