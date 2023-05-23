37bc3bbc7d0e9f519858209c5e9b981b3fc7fe22
Ayosef <syrivera@uc.cl>
2023-05-23 12:49:11 -0400

    Refactor book constructor to class

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const addBtn = document.querySelector('#add-btn');
 
 let myLibrary = [];
 
-function Book(title, author, pages, read) {
-    this.title = title,
-    this.author = author,
-    this.pages = pages,
-    this.read = read
+class Book {
+    constructor (title,author,pages,read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 function addBookToLibrary(title, author, pages, read) {
